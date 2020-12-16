40134a2f31b9c710a009256ce039196d92ba9982
Danil Soldatov <of.swg.csgo@gmail.com>
2020-12-16 20:56:19 +0700

    Init

---
+++ cookie.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+void cookie()
+{
+    FILE *stat;
+    stat = fopen("cookies.txt", "a+");
+
+    long int ttime;
+
+    ttime = time(NULL);
+    fprintf(stat, "connected: ");
+    fprintf(stat, ctime(&ttime));
+    fprintf(stat, "\n");
+    fclose(stat);
+}
